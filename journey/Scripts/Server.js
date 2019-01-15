3b9d4e7f827a276157fcd047263dedd443bef430
ifwatstylers <ifwat.ibrahim@plus.uemnet.com>
2019-01-15 16:37:49 +0800

    15 Jan 1637
    Change all button to images

---
+++ journey/Scripts/Server.js
@@ -0,0 +1,38 @@
+﻿var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'admin_festive',
+        password: 'P@55w0rd2018',
+        server: 'tcp:festive.database.windows.net,1433',
+        database: 'FestiveDB'
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+        // query to the database and get the records
+        request.query('select * from Student', function (err, recordset) {
+
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
